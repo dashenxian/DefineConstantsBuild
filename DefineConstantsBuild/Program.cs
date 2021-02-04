29de8d199738a46bd148d9bae14a4baef68dc2ae
1747623132@qq.com <1747623132@qq.com>
2021-02-04 17:54:53 +0800

    添加项目文件。

---
+++ DefineConstantsBuild/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+#if LabA
+using LabA;
+#elif LabB
+using LabB;
+#endif
+
+namespace DefineConstantsBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = Class1.Add(1, 2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabA/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabA
+{
+    public class Class1
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
